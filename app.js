6fc37fba81ddac1ed5dc5e0ed138145daffc18a6
akshay <akshay.rachapudi@gmail.com>
2021-02-20 07:30:35 -0500

    Added some basic css and js

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme')
+
+    var className = document.body.className;
+    if(className == "dark-theme") {
+        this.textContent = "Light";
+    }
+    else {
+        this.textContent = "Dark";
+    }
+    console.log('current class name: ' + className);
+});
